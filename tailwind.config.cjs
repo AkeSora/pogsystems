16fb65315a647d177ec61c8c5830d0f6fd0e9b04
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-12-08 20:59:14 +0100

    feat: improve project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,42 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
+// @ts-check
+
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
+const gray = colors.zinc;
+
+const pogsystems = plugin(({ addUtilities }) => {
+  addUtilities({
+    ".bg-pattern": {
+      "background-image": "radial-gradient(#ffffff10 1.5px, transparent 0)",
+      "background-size": "25px 25px",
+    },
+  });
+});
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	plugins: [],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Inter", "Comic Sans MS", ...defaultTheme.fontFamily.sans]
-			}
-		},
-	},
-}
+const tailwindCssConfig = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [pogsystems],
+  theme: {
+    colors: {
+      amber: colors.amber,
+      "amber-muted": "#5c4d28",
+      black: colors.black,
+      current: colors.current,
+      gray,
+      orange: colors.orange,
+      transparent: colors.transparent,
+      yellow: colors.yellow,
+      white: colors.white,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+};
+
+module.exports = tailwindCssConfig;
